a2b07d9afbf2b60021a9b78db1414626548317cf
unknown <ssralphks@gmail.com>
2023-02-28 12:27:22 +0700

    laraveldb working

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_Admin',
+        'photo'
     ];
 
     /**
@@ -41,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function attendances()
+    {
+        return $this->hasMany(comment::class);
+    }
 }
